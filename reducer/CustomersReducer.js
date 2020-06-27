bd701244255b81b8d8d1ba5e5722a51da57f551b
Abdul Mobeen <mobeendev@gmail.com>
2020-06-27 20:15:43 +0300

    setup customer contex

---
+++ reducer/CustomersReducer.js
@@ -0,0 +1,12 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "REMOVE_CUSTOMER":
+
+    case "ADD_CUSTOMER":
+
+    case "EDIT_CUSTOMER":
+
+    default:
+      return state;
+  }
+};
